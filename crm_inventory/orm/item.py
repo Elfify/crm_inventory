43e2047eb8c8d1cac94775211a0f3fc2433ee904
Adam Bailey <ab67319@gmail.com>
2024-04-26 11:22:27 -0600

    ELF Refactor: Add inventory_id column

--- crm_inventory/orm/item.py
+++ crm_inventory/orm/item.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, UniqueConstraint
+from sqlalchemy import ForeignKey, Integer, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from crm_inventory.orm.base import Base
@@ -9,15 +9,16 @@ class Item(Base, TimestampMixin):
     __tablename__ = "items"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    inventory_id: Mapped[int] = mapped_column(Integer, ForeignKey('inventories.id'))
     SKU: Mapped[str]
     name: Mapped[str] = mapped_column(String(length=75))
-    category: Mapped[str]
+    category: Mapped[str] = mapped_column(String, nullable=True)
     cost: Mapped[float]
     single_price: Mapped[float]
     total_quantity: Mapped[int] = mapped_column(Integer)
     current_quantity: Mapped[int] = mapped_column(Integer)
     stock_item: Mapped[bool]
-    image_url: Mapped[str]
+    image_url: Mapped[str] = mapped_column(String, nullable=True)
 
-    inventory_id: Mapped[int] = relationship("Inventory", back_populates="items")
-    __table_args__ = (UniqueConstraint('sku', 'inventory_id', name='_inventory_sku_uc'),)
+    inventory: Mapped[int] = relationship("Inventory", back_populates="items")
+    __table_args__ = (UniqueConstraint('SKU', 'inventory_id', name='_inventory_sku_uc'),)
