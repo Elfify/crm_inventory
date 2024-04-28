5227d04a0d659866450de7241334481d6637b584
Adam Bailey <ab67319@gmail.com>
2024-04-28 11:26:50 -0600

    ELF Feat: Add engine initialization on app start

--- crm_inventory/app.py
+++ crm_inventory/app.py
@@ -2,15 +2,17 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from crm_inventory.config.config import initialize_db
-
-app = FastAPI()
+from crm_inventory.utils.db import initialize_db, initialize_engine
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     initialize_db()
+    initialize_engine()
     yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/health")
--- crm_inventory/utils/db.py
+++ crm_inventory/utils/db.py
@@ -1,10 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
 from crm_inventory.config.config import DATABASE_URL
 from crm_inventory.data.exceptions import DatabaseInitializationException, DatabaseEngineException
 
+
+_engine: Engine | None = None
+TransactionalSession = sessionmaker(_engine)
 
 def initialize_db():
     try:
@@ -22,6 +25,8 @@ def initialize_engine():
     try:
         global _engine
         _engine = create_engine(url=DATABASE_URL, pool_pre_ping=True)
+        TransactionalSession.configure(bind=_engine)
+
         print("Engine initialized!")
     except Exception:
         raise DatabaseEngineException
