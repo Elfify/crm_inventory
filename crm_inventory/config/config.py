b47084832e5a2f8e4b6ea379de536021909005af
Adam Bailey <ab67319@gmail.com>
2024-04-28 11:25:49 -0600

    ELF Refactor: Update config for only db_url

--- crm_inventory/config/config.py
+++ crm_inventory/config/config.py
@@ -1,13 +1,1 @@
-from sqlalchemy_utils import create_database, database_exists
-
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/crm_inventory"
-
-
-def initialize_db():
-    print("Initializing database...")
-
-    if database_exists(DATABASE_URL):
-        print("Database exists!")
-    else:
-        create_database(DATABASE_URL)
-        print("Database created!")
+DATABASE_URL = "postgresql://postgres:postgres@postgres-local:5432/crm_inventory"
