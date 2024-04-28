da6979e2e47b3e7e05ed5d1d491bcc7b6db28306
Adam Bailey <ab67319@gmail.com>
2024-04-28 11:13:34 -0600

    ELF Feat: Add engine initialization

--- crm_inventory/utils/db.py
+++ crm_inventory/utils/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
 from crm_inventory.config.config import DATABASE_URL
-from crm_inventory.data.exceptions import DatabaseInitializationException
+from crm_inventory.data.exceptions import DatabaseInitializationException, DatabaseEngineException
 
 
 def initialize_db():
@@ -15,5 +15,13 @@ def initialize_db():
         else:
             create_database(DATABASE_URL)
             print("Database created!")
-    except Exception as e:
+    except Exception:
         raise DatabaseInitializationException
+
+def initialize_engine():
+    try:
+        global _engine
+        _engine = create_engine(url=DATABASE_URL, pool_pre_ping=True)
+        print("Engine initialized!")
+    except Exception:
+        raise DatabaseEngineException
