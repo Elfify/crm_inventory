a9b2c5ca4d16731267e777a80b651453a6ef0a22
Adam Bailey <ab67319@gmail.com>
2024-04-29 14:46:00 -0600

    Test: Add pytest fixtures

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,24 @@
 import pytest
+from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
+
+from crm_inventory.app import app
+from crm_inventory.utils.db import get_session, TransactionalSession
+from tests.utils.db import clean_database
+
 
 @pytest.fixture
 def db_engine():
-    engine = create_engine("postgresql://postgres:postgres@localhost:5432/crm_inventory")
+    engine = create_engine("postgresql://postgres:postgres@localhost:5432/crm_inventory-test")
 
     return engine
+
+@pytest.fixture
+def test_client() -> TestClient:
+    return TestClient(app)
+
+@pytest.fixture
+def initialize_and_clean_db():
+    session = TransactionalSession()
+    yield session
+    clean_database(session)
