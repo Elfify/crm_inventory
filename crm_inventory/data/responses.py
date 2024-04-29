90f34d3caf83558431bee3ec87b595d0eb2a1322
Adam Bailey <ab67319@gmail.com>
2024-04-28 18:23:48 -0600

    Feat: Add inventory response

---
+++ crm_inventory/data/responses.py
@@ -0,0 +1,26 @@
+from typing import Generic, Optional, TypeVar
+
+from pydantic import BaseModel
+
+from crm_inventory.data.inventory import Inventory
+from crm_inventory.data.item import Item
+
+
+ModelType = TypeVar('ModelType', bound=BaseModel)
+
+
+class BaseResponse(BaseModel, Generic[ModelType]):
+    result: str = "success"
+    error: Optional[str] = None
+    data: Optional[ModelType] = None
+
+    class Config:
+        exclude_none = True
+
+
+class InventoryResponse(BaseResponse[Inventory]):
+    pass
+
+
+# class ItemResponse(BaseResponse[Item]):
+#     pass
